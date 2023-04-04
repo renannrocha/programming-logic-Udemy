2929c78d94f1f2e0d28fad1165229eda16d24fb8
renannrocha <renanrocha8897@gmail.com>
2023-04-04 15:46:52 -0300

    inicio linguagemC

---
+++ linguagem_C/pratica_aulas/teste01_helloword.c
@@ -0,0 +1,22 @@
+// a primeira coisa que basica de um progama em C e a inclusão de bibliotecas :
+
+#include <stdio.h> // stdio.h e a biblioteca responsavel pelas funções de entrada e saida de dados em C
+
+// int main(){  }  =  esse padrão de assinatura ele indentifica o entryPoint do meu progama, ou seja é aonde o progama vai começar
+// tudo que voçê colocar entre as chaves vai ser o progama propiamente dito.
+int main() {
+    // comando de saida de dados
+    printf("Ola, Mundo !"); // ATENÇÃO SEMPRE COLOCAR ; no final de cada função !
+
+
+    return 0; // sempre no final colocar esse comando return 0; pois na lingua C por padrão quando um progama termina sua execução,           // ele vai informar um codigo de erro, esse codigo ele vai ser 0 se não haver erro, ou diferente de 0 se ouver erro.
+}
+
+
+// esqueleto basico de um progama em C:
+
+//int main() {
+//
+//
+//    return0;
+//}
