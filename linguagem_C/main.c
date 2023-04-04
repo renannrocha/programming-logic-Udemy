4a4bd45eedd06caf7d50dfc326b6d96fcada57d1
renannrocha <renanrocha8897@gmail.com>
2023-04-04 00:21:59 -0300

    ecg

---
+++ linguagem_C/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+    setlocate(LC_ALL, "Portuguese");
+
+    int x, y;
+    printf("x: ");
+    scanf("%d", &x);
+    printf("y: ");
+    scanf("%d", &y);
+
+    if ( x > y ) {
+        printf("O maior numero e x = %d\n", x);
+    } else {
+        printf("O maior numero é y = %d\n", y);
+    }
+
+    system("pause");
+    return 0;
+}
