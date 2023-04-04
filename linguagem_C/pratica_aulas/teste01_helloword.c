2ae40d9d6586500d13e44ae7285d04de8979a1dd
renannrocha <renanrocha8897@gmail.com>
2023-04-04 15:49:19 -0300

    edit code

--- linguagem_C/pratica_aulas/teste01_helloword.c
+++ linguagem_C/pratica_aulas/teste01_helloword.c
@@ -1,15 +1,16 @@
-// a primeira coisa que basica de um progama em C e a inclusão de bibliotecas :
+// a primeira coisa que basica de um progama em C e a inclusï¿½o de bibliotecas :
 
-#include <stdio.h> // stdio.h e a biblioteca responsavel pelas funções de entrada e saida de dados em C
+#include <stdio.h> // stdio.h e a biblioteca responsavel pelas funï¿½ï¿½es de entrada e saida de dados em C
 
-// int main(){  }  =  esse padrão de assinatura ele indentifica o entryPoint do meu progama, ou seja é aonde o progama vai começar
-// tudo que voçê colocar entre as chaves vai ser o progama propiamente dito.
+// int main(){  }  =  esse padrï¿½o de assinatura ele indentifica o entryPoint do meu progama, ou seja ï¿½ aonde o progama vai comeï¿½ar
+// tudo que voï¿½ï¿½ colocar entre as chaves vai ser o progama propiamente dito.
 int main() {
     // comando de saida de dados
-    printf("Ola, Mundo !"); // ATENÇÃO SEMPRE COLOCAR ; no final de cada função !
+    printf("Ola, Mundo !"); // ATENï¿½ï¿½O SEMPRE COLOCAR ; no final de cada funï¿½ï¿½o !
 
 
-    return 0; // sempre no final colocar esse comando return 0; pois na lingua C por padrão quando um progama termina sua execução,           // ele vai informar um codigo de erro, esse codigo ele vai ser 0 se não haver erro, ou diferente de 0 se ouver erro.
+    return 0; // sempre no final colocar esse comando return 0; pois na lingua C por padrï¿½o quando um progama termina sua execuï¿½ï¿½o,           
+              // ele vai informar um codigo de erro, esse codigo ele vai ser 0 se nï¿½o haver erro, ou diferente de 0 se ouver erro.
 }
 
 
