eaf406e9a7e0789ed2c95b942fb501b1a3c791a2
renannrocha <renanrocha8897@gmail.com>
2023-04-06 21:03:31 -0300

    estrutura repita ate

---
+++ linguagem_C/estrutura repitaAte/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void limpar_entrada()
+{
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+int main()
+{
+    char resp;
+    double C, F;
+
+    do {
+      printf("Digite a temperatura em Celcius: ");
+      scanf("%lf", &C);
+      F = 9.0 * C / 5.0 + 32.0;
+      printf("Equivalente em Fahrenheit: %.1lf\n", F);
+      printf("Deseja repetir (s/n)? ");
+      limpar_entrada();
+      scanf("%c", &resp);
+    } while(resp == 's');
+
+    return 0;
+}
