4d92b1df80b94df84c83fb4d687f917ed612826b
renannrocha <renanrocha8897@gmail.com>
2023-04-08 20:47:09 -0300

    diagonal_negativos edit

--- linguagem_Python/exercicios/diagonal_negativos.py
+++ linguagem_Python/exercicios/diagonal_negativos.py
@@ -1,10 +1,10 @@
 m = int(input("Qual a ordem da matriz ? "))
-n = m
 
-mat = [[0 for x in range(0, m)] for x in range(0, n)]
+
+mat = [[0 for x in range(0, m)] for x in range(0, m)]
 
 for i in range(0, m):
-    for j in range(0, n):
+    for j in range(0, m):
         mat[i][j] = int(input(f"Elemento [{i},{j}]: "))
 
 print()
@@ -14,7 +14,7 @@ for i in range(0, m):
 
 negativos = 0
 for i in range(0, m):
-    for j in range(0, n):
+    for j in range(0, m):
         if mat[i][j] < 0:
             negativos = negativos + 1
 print()
