c30bcff9a0d60cd277976d003d25c97b55ca6f72
renannrocha <renanrocha8897@gmail.com>
2023-05-03 19:39:00 -0300

    firt algoritm c++

---
+++ linguagem_C++/first/firstalgoritmc++/main.cpp
@@ -0,0 +1,12 @@
+/**< iostream - biblioteca de entrada e saida c++ */
+#include <iostream>
+
+using namespace std;
+/**< namespace - organizar seus arquivos e bibliotecas do seu projeto*/
+
+int main()
+{
+    /**< cout - comando de saida de dados */
+    cout << "Hello world!" << endl;
+    return 0;
+}
