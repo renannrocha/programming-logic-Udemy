d79e4b184f401ca79fe2f76c95a90befed3ed860
renannrocha <renanrocha8897@gmail.com>
2023-05-23 13:10:56 -0300

    casting cpp

---
+++ linguagem_C++/aulas/casting cpp/casting cpp/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // casting em c++
+
+    double b1, b2, h, area;
+    b1 = 6.0;
+    b2 = 8.0;
+    h = 5.0;
+    area = (b1 + b2) / 2.0 * h;
+    cout << fixed << setprecision(2);
+    cout << area << endl;
+
+    return 0;
+}
