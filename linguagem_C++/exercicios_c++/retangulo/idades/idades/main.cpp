b756e80d9fe1547343b4f11a140f6c8bfb9e0990
renannrocha <renanrocha8897@gmail.com>
2023-05-23 19:03:08 -0300

    exe idades cpp

---
+++ linguagem_C++/exercicios_c++/retangulo/idades/idades/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string nome1, nome2;
+    int idade1, idade2;
+    double media;
+
+    cout << "digite o nome da .1a pessoa: ";
+    getline(cin, nome1);
+    cout << "digite a idade da .1a pessoa: ";
+    cin >> idade1;
+
+    cout << "\nDigite o nome da .2a pessoa: ";
+    cin.ignore(INT_MAX,'\n');
+    getline(cin, nome2);
+    cout << "digite a idade da .2a pessoa: ";
+    cin >> idade2;
+
+    media = idade1 + idade2 / 2;
+
+    cout << fixed << setprecision(2);
+    cout << "\na idade media de " << nome1 << " e "
+         << nome2 << " e " << media << endl;
+
+
+    return 0;
+}
