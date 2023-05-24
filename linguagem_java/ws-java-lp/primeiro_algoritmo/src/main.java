b6f82a13cd00e8197901cdd8a09669b1dfa14f34
renannrocha <renanrocha8897@gmail.com>
2023-05-24 13:40:45 -0300

    java module

---
+++ linguagem_java/ws-java-lp/primeiro_algoritmo/src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola mundo!");
+		
+	}
+
+}
+// java e uma linguagem orientada a objetos, todo o progama tem que estar dentro de uma classe
