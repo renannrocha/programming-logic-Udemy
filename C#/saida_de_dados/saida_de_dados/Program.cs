1d01952979cbe8c89312638020faa5db24be056d
renannrocha <renanrocha8897@gmail.com>
2023-06-21 23:18:13 -0300

    csharp study

---
+++ C#/saida_de_dados/saida_de_dados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace saida_de_dados {
+    internal class Program {
+        static void Main(string[] args) {
+            // saida de dados - onde writeline(com quebra de linha) e write(sem quebra de linha)
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int idade = 32;
+            double salario = 4560.9;
+            string nome = "Maria Silva";
+            char sexo = 'F';
+
+            Console.WriteLine("A funcionaria " + nome + ", sexo " 
+            + sexo + ", ganha " + salario.ToString("F2", CI) 
+            + " e tem " + idade + " anos");
+        }
+    }
+}
