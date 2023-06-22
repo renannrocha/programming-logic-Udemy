86add183f2b0e3eef601a70e5c27e785514b129b
renannrocha <renanrocha8897@gmail.com>
2023-06-21 23:02:01 -0300

    csharp study

---
+++ C#/primeiro_algoritmo/primeiro_algoritmo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiro_algoritmo
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+        }
+    }
+}
